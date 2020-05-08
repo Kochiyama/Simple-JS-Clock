0e4170091e3151b7d0a34ad90060519690cf0700
Marcelo Haruo Kochiyama <marcelokochiyama2210@gmail.com>
2020-05-08 03:52:50 -0300

    Updating comments Clock.js

--- clock.js
+++ clock.js
@@ -1,8 +1,12 @@
 //clock implementation
 
-//define all the necessary variables and a second constant
+//define all the necessary variables to store acutal dates
 let actualDate, hours, minutes, seconds, weekday
+
+//define a constant for the miliseconds into one second
 const second = 1000
+
+//define a list of the week days from the first day (access by [0]) to the last day (access by [6])
 const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
 //get clock structure on html
@@ -16,14 +20,20 @@ function updateClock() {
   actualDate = new Date()
 
   //for  weekday updates
+  // store the actual week day, getting the list item from position of the actual day
+  //cause the function getDay returns the number of the week day (0 to 6), so the list is in the corresponding order
+  //and we can use it like the index of the list
   weekday = daysOfWeek[actualDate.getDay()]
+  
+  //if the text into the html structure day is different than the current day of the week
   if (day.innerHTML != weekday) {
     day.innerHTML = weekday
   }
   
 
   //split in hour, minute and second
-  //and if it is not greater or equal to ten it wont have the left zero (like 1, 2, 3 and we want 01, 02, 03), so put a zero at the left
+  //and if it is not greater or equal to ten it wont have the left zero (like 1, 2, 3 and we want 01, 02, 03)
+  //so put a zero at the left
 
   hours = actualDate.getHours()
   if (hours < 10) {
@@ -47,7 +57,7 @@ function updateClock() {
   clock.innerHTML = time
 }
 
-//remove 1 second delay to first update of clock
+//remove the 1 second delay of first update (when the page loads)
 updateClock()
 
 //infinite loop that executes the updateClock function every second(declarated as a const at the top)
